6f6f53c566e1789d18bbc86aa4b051f77212fc1b
HasanKulbuzhev <ashatkfhcj@gmail.com>
2023-03-29 23:35:37 +0300

    add Theme, Language, Example, Word models and migrations

---
+++ database/migrations/2023_03_29_200837_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->foreignId('language_id')->constrained('languages')->cascadeOnDelete();
+            $table->foreignId('theme_id')->constrained('themes')->cascadeOnDelete();
+            $table->foreignId('original_word_id')->constrained('words')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
